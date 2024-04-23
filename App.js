7442d3d1a0ba92781b6579c3914553493f339dcb
Volodymyr Pavlyshyn <pavlyshyn@gmail.com>
2024-04-23 10:52:03 +0200

    op-sample

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {open} from '@op-engineering/op-sqlite'
 
+
+function generateUUID() { // Public Domain/MIT
+  var d = new Date().getTime();//Timestamp
+  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16;//random number between 0 and 16
+      if(d > 0){//Use timestamp until depleted
+          r = (d + r)%16 | 0;
+          d = Math.floor(d/16);
+      } else {//Use microseconds since page-load if supported
+          r = (d2 + r)%16 | 0;
+          d2 = Math.floor(d2/16);
+      }
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
 export default function App() {
+  const dbname = `${ generateUUID()}-testdb.sqlite`
+
+    const db = open({name: dbname})
+
+    const version =  db.execute('select sqlite_version();')
+    console.log('db name', dbname, 'version  ', version.rows)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> DB version {JSON.stringify(version)}</Text>
       <StatusBar style="auto" />
     </View>
   );
